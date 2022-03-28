ff506636552c58be67d99915f58d34c5d197de93
vlutsenko <vyacheslav.lutsenko@gmai.com>
2022-03-28 18:45:48 +0300

    updated + ansible

--- renderer.py
+++ renderer.py
@@ -1,13 +1,16 @@
 import re
 
 ###templates for file
+templ_ansible_begin = "ansible all -i hosts -m shell -a \""
 templ_begin = "docker run -it --rm --pull always ghcr.io/porthole-ascend-cinnamon/mhddos_proxy:latest "
 templ_end = " -t 1000 -p 1200 --rpc 1000  --debug"
+templ_ansible_end = "\"--extra-vars \"ansible_user=root ansible_password=@Ubuntu123a\" "
 
 file1 = open('raw_file_test', 'r')
 Lines = file1.readlines()
 
 ###start file
+print(templ_ansible_begin, end=" ")
 print(templ_begin, end=" ")
 
 ###for line in Lines1:
@@ -27,3 +30,4 @@ file1.close()
 
 ###end file
 print (templ_end, end=" ")
+print(templ_ansible_end, end=" ")
