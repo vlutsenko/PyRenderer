28e55c5a6787e0c6302b0d7e8ace9a35fc13d0c3
vlutsenko <vyacheslav.lutsenko@gmail.com>
2022-03-30 13:09:34 -0700

    j2 test fixed

--- check_templates.py
+++ check_templates.py
@@ -2,13 +2,16 @@ import jinja2
 
 ip_addr = ["10.11.11.1", "12.12.12.3", "13.22.11.2", "19.33.1.3"]
 
-var = { 
-    "ip": ip_addr
+var_val = { 
+    "ip_addr": ip_addr
 }
 
+
+
 route_templ = '''
-  ip route {%- for address in ip_addr %}
-  {%- endfor}
+  {%- for addr in ip_addr %}
+  ip route {{ addr }}/32
+  {%- endfor %}
 '''
 t = jinja2.Template(route_templ)
-print (t.render(var))
+print (t.render(var_val))
