1e04186784812d8dcb856bc30b0d20e1be11a0ee
vlutsenko <vyacheslav.lutsenko@gmai.com>
2022-07-03 15:43:59 +0300

    simplified rendering

--- renderer.py
+++ renderer.py
@@ -1,17 +1,38 @@
+from asyncio.windows_events import NULL
 import re
+from pprint import pprint
 
 ###templates for file
 templ_begin = "docker run -it --rm --pull always ghcr.io/porthole-ascend-cinnamon/mhddos_proxy:latest "
 templ_end = " -t 1000 --rpc 1000  --debug"
 
-file1 = open('raw_file_test', 'r')
-Lines = file1.readlines()
+#docker_in_txt = NULL
+#docker_in = str(docker_in_txt)
+docker_in =[]
+
+with open('clear', 'r') as clear_set:
+   file0 = clear_set.readlines()
+
+print(type(file0))
+
+for line in file0:
+   #pprint(line)
+   if ("http" or "https") in line:
+      #pprint(line)
+      next
+   else:
+      docker_in.append(line)   
+
+   
+
+# with open('raw_file_test', 'r') as file1:
+#    Lines = file1.readlines()
 
 ###start file
 print(templ_begin, end=" ")
 
 ###for line in Lines1:
-for line in Lines:
+for line in docker_in:
       result = re.findall("[0-9]+.[0-9]+.[0-9]+.[0-9]+|[0-9]+|tcp|udp", line.strip())
       if len(result) > 1:
          #print(result)
@@ -23,7 +44,7 @@ for line in Lines:
             print (result[r+1]+"://"+result[0]+":"+result[r], end=" ")
             r += 2
 
-file1.close()
+#file1.close()
 
 ###end file
 print (templ_end, end=" ")
